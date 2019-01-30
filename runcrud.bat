185d85d6ad284939a734bd4fbc18d61ca50d0f99
szymonstuszek <szymonstuszek@gmail.com>
2019-01-30 15:10:56 +0100

    writing .bat file

---
+++ runcrud.bat
@@ -0,0 +1,28 @@
+call gradlew build
+if "%ERRORLEVEL%" == "0" goto rename
+echo.
+echo GRADLEW BUILD has errors - breaking work
+goto fail
+
+:rename
+del build\libs\crud.war
+ren build\libs\tasks-0.0.1-SNAPSHOT.war crud.war
+if ""%ERRORLEVEL%" == "0" goto stoptomcat
+echo Cannot rename file
+goto fail
+
+:stoptomcat
+call %CATALINA_HOME%\bin\shutdown.bat
+
+:copyfile
+@rem
+
+goto end
+
+:fail
+echo.
+echo There were errors
+
+:end
+echo.
+echo Work is finished.
