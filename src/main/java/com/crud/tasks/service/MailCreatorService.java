85114c10b9503772e9d627542dc8e87e385dcb2d
szymonstuszek <szymonstuszek@gmail.com>
2019-03-21 19:17:57 +0100

    Task 24.2

---
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -0,0 +1,34 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class MailCreatorService {
+
+    @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
+    @Qualifier("templateEngine")
+    private TemplateEngine templateEngine;
+
+    public String buildTrelloCardEmail(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/tasks_frontend");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye", "Thanks for using tasks app!");
+        context.setVariable("company_name", adminConfig.getAdminName());
+        context.setVariable("company_goal", adminConfig.getCompanyGoal());
+        context.setVariable("company_email", adminConfig.getCompanyMail());
+        context.setVariable("company_phone", adminConfig.getCompanyPhone());
+
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
+}
--- src/main/java/com/crud/tasks/service/SimpleMailService.java
+++ src/main/java/com/crud/tasks/service/SimpleMailService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -19,11 +21,13 @@ public class SimpleMailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private MailCreatorService mailCreatorService;
+
     public void send(final Mail mail) {
         LOGGER.info("Starting email preparation...");
         try{
-            SimpleMailMessage mailMessage = createMailMessage(mail);
-            javaMailSender.send(mailMessage);
+            javaMailSender.send(createMimeMessage(mail));
 
             LOGGER.info("Email has been sent.");
         } catch (MailException e) {
@@ -35,13 +39,16 @@ public class SimpleMailService {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
-        mailMessage.setText(mail.getMessage());
-
-        mailMessage.setCc(new String[0]);
-
-        Optional.ofNullable(mail.getToCc())
-                .ifPresent(mailMessage::setCc);
-
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
         return mailMessage;
+    }
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
     }
 }
